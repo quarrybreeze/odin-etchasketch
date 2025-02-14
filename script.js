8bceac80d0ef02f38f028c576cb5235fedc48f3d
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-13 17:16:10 -0800

    set up grid of 16x16 boxes

---
+++ script.js
@@ -0,0 +1,14 @@
+const div = document.getElementById("screen")
+
+for (var h = 0; h < 16; h++) {
+    for (var w = 0; w < 16; w++) {
+        const box = document.createElement("div");
+        box.style.width = "8px"
+        box.style.height = "8px" //creates a 8x8 square
+        box.style.border = "thin solid black" //creates a 1px border, resulting in 10px square
+        box.style.backgroundColor = "lightgray"
+        div.appendChild(box);
+    }
+}
+
+
