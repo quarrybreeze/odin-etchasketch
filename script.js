44ecd46368008913f0d2b2c893ca63d8e3e10970
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-13 17:27:32 -0800

    added hover color feature for step 3

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const div = document.getElementById("screen")
 
+function boxColor(box) {
+    box.style.backgroundColor = "blue";
+}
+
 for (var h = 0; h < 16; h++) {
     for (var w = 0; w < 16; w++) {
         const box = document.createElement("div");
-        box.style.width = "8px"
-        box.style.height = "8px" //creates a 8x8 square
-        box.style.border = "thin solid black" //creates a 1px border, resulting in 10px square
-        box.style.backgroundColor = "lightgray"
+        box.style.width = "8px";
+        box.style.height = "8px"; //creates a 8x8 square
+        box.style.border = "thin solid black"; //creates a 1px border, resulting in 10px square
+        box.style.backgroundColor = "lightgray";
         div.appendChild(box);
     }
 }
 
+const pixels = document.querySelectorAll("div");
 
+pixels.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+        boxColor(box);
+    });
+});
